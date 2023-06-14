55eebeed288cb84243b20f47140587edbf642842
jijamesji00x <91174069+jijamesji00x@users.noreply.github.com>
2023-06-14 22:07:08 +0700

    copy project to git folder

---
+++ backend/index.js
@@ -0,0 +1,94 @@
+const { log } = require("console");
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+const { Client } = require("pg");
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "pegasus",
+  password: "",
+  port: 5432,
+});
+
+client.connect();
+
+/*client.query(`Select * from country_and_capitals`, (err, res) => {
+  if (!err) {
+    console.log(res.rows);
+  } else {
+    console.log(err.message);
+  }
+  client.end;
+});*/
+
+/*router.get("/", function (req, res, next) {
+  client.query(`Select * from country_and_capitals`, (err, res) => {
+    if (!err) {
+      res.send(result.rows);
+      console.log(res.rows);
+    } else {
+      console.log(err.message);
+    }
+    client.end;
+  });
+});*/
+/*const data = [
+  {
+    country: "Thailand",
+    capital: "Bangkok",
+  },
+  {
+    country: "China",
+    capital: "Beijing",
+  },
+  {
+    country: "Japan",
+    capital: "Tokyo",
+  },
+  {
+    country: "France",
+    capital: "Paris",
+  },
+];*/
+
+/*router.get("/data", function (req, res, next) {
+  res.send(data);
+});*/
+
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../frontend", "/index.html"));
+});
+
+//console.log(process.env);
+router.get("/data", function (req, res, next) {
+  console.log("test");
+  client.query(`Select * from country_and_capitals`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+      console.log(result.rows);
+      console.log("api/data success");
+    } else {
+      console.log(err.message);
+    }
+
+    client.end;
+  });
+});
+/*client.query(`Select * from country_and_capitals`, (err, res) => {
+  if (!err) {
+    //res.send(result.rows);
+    console.log(res.rows);
+    //res.sendFile(path.join(__dirname, "../frontend", "/index.html"));
+  } else {
+    console.log(err.message);
+  }
+  client.end;
+});*/
+app.use("/", router);
+app.listen(3001);
+console.log("server running in port 3001");
---
+++ sql_scripts/1.sql
@@ -0,0 +1,16 @@
+psql -U postgres;
+create database pegasus;
+create table country_and_capitals (country text, capital text);
+
+insert into country_and_capitals(country,capital) values(
+    'Thailand','Bangkok'
+);
+insert into country_and_capitals(country,capital) values(
+    'China','Beijing'
+);
+insert into country_and_capitals(country,capital) values(
+    'Japan','Tokyo'
+);
+insert into country_and_capitals(country,capital) values(
+    'France','Paris'
+);
