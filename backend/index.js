280809bf7e7b98dde89848d6800d38fbc3973a6e
jijamesji00x <91174069+jijamesji00x@users.noreply.github.com>
2023-06-27 21:23:41 +0700

    edit

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,11 @@ require("dotenv").config();
 
 const { Client } = require("pg");
 const client = new Client({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 client
